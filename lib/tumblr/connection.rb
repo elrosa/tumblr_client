175a0f63cf456cb177b6fec732693e2de1fe467d
Magda Sikorska <madzia.sikorska@gmail.com>
2014-06-13 16:23:24 +0200

    tumblr client

--- lib/tumblr/connection.rb
+++ lib/tumblr/connection.rb
@@ -16,15 +16,16 @@ module Tumblr
 
       #client = options[:client] ||= Faraday.default_adapter
 
-      Faraday.new("http://#{api_host}/", default_options.merge(options)) do |conn|
+      Faraday.new("http://#{api_host}/", default_options.merge(options)) do |faraday|
         data = { :api_host => api_host }.merge(credentials)
         unless credentials.empty?
-          conn.request :oauth, data
+          faraday.request :oauth, data
         end
-        conn.request :multipart
-        conn.request :url_encoded
-        conn.response :json, :content_type => /\bjson$/
-        #conn.adapter client
+
+        faraday.request  :multipart
+        faraday.request  :url_encoded
+        faraday.response :json, :content_type => /\bjson$/
+        faraday.adapter  Faraday.default_adapter
       end
     end
 
